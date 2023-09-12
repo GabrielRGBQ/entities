4aa44f274aa90a859e1ee19582bec983dd1460db
Gabriel <gabri.dlq@gmail.com>
2023-09-12 08:58:09 +0200

    Add CORS policy

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from . import models
-from .database import engine
-from .routers import entity, user, authentication, like
 from .config import settings
+from .database import engine
+from .routers import authentication, entity, like, user
 
 # Create the tables in the database if they don't already exist
 # It is no longer needed since we incorporated Alembic... we can create the
@@ -11,6 +12,16 @@ from .config import settings
 # models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+# Handle CORS policy
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(entity.router)
 app.include_router(user.router)
