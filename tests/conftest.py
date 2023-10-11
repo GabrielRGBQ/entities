5eea927e0170296e22c4c6da965b2c52353d83a5
Gabriel <gabri.dlq@gmail.com>
2023-10-11 15:07:19 +0200

    Create all the tests for Entities

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,13 +6,16 @@ from sqlalchemy.orm import sessionmaker
 
 from app.config import settings
 from app.database import Base, get_db
+from app import models
 from app.main import app
+from app.oauth2 import create_access_token
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 
 @pytest.fixture
 def session():
@@ -24,6 +27,7 @@ def session():
     finally:
         db.close()
 
+
 @pytest.fixture
 def client(session):
     # Here is what is run before the test has been run
@@ -32,10 +36,24 @@ def client(session):
             yield session
         finally:
             session.close()
+
     app.dependency_overrides[get_db] = override_get_db
     yield TestClient(app)
     # Here is what is run after the test has been run
     # Base.metadata.drop_all(bind=engine)
+
+
+@pytest.fixture
+def test_user2(client):
+    user_data = {"email": "sanjeev123@gmail.com", "password": "password123"}
+    res = client.post("/users/", json=user_data)
+
+    assert res.status_code == 201
+
+    new_user = res.json()
+    new_user["password"] = user_data["password"]
+    return new_user
+
 
 @pytest.fixture
 def test_user(client):
@@ -45,3 +63,52 @@ def test_user(client):
     new_user = res.json()
     new_user["password"] = user_data["password"]
     return new_user
+
+
+@pytest.fixture
+def token(test_user):
+    return create_access_token({"user_id": test_user["id"]})
+
+
+@pytest.fixture
+def authorized_client(client, token):
+    client.headers = {**client.headers, "Authorization": f"Bearer {token}"}
+    return client
+
+
+@pytest.fixture
+def test_entities(test_user, test_user2, session):
+    entities_data = [
+        {
+            "title": "Entity 1",
+            "description": "Description of entity 1",
+            "owner_id": test_user["id"],
+        },
+        {
+            "title": "Entity 2",
+            "description": "Description of entity 2",
+            "owner_id": test_user["id"],
+        },
+        {
+            "title": "Entity 3",
+            "description": "Description of entity 3",
+            "owner_id": test_user["id"],
+        },
+        {
+            "title": "Entity 4",
+            "description": "Description of entity 4",
+            "owner_id": test_user2["id"],
+        }
+    ]
+
+    def create_entity_model(entity):
+        return models.Entity(**entity)
+
+    entity_map = map(create_entity_model, entities_data)
+    entities = list(entity_map)
+
+    session.add_all(entities)
+    session.commit()
+
+    entities = session.query(models.Entity).all()
+    return entities
---
+++ tests/test_entities.py
@@ -0,0 +1,122 @@
+import pytest
+from app import schemas
+
+
+def test_get_all_entities(authorized_client, test_entities):
+    res = authorized_client.get("/entities/")
+    assert len(res.json()) == len(test_entities)
+    assert res.status_code == 200
+
+
+def test_unauthorized_user_get_all_entities(client):
+    res = client.get("/entities/")
+    assert res.status_code == 401
+
+
+def test_unauthorized_user_get_one_entity(client, test_entities):
+    res = client.get(f"/entities/{test_entities[0].id}")
+    assert res.status_code == 401
+
+
+def test_get_one_entity_not_exist(authorized_client):
+    res = authorized_client.get("/entities/88888")
+    assert res.status_code == 404
+
+
+def test_get_one_entity(authorized_client, test_entities):
+    res = authorized_client.get(f"/entities/{test_entities[0].id}")
+    entity = schemas.EntityOut(**res.json())
+    assert entity.Entity.id == test_entities[0].id
+    assert entity.Entity.description == test_entities[0].description
+    assert entity.Entity.title == test_entities[0].title
+
+
+@pytest.mark.parametrize(
+    "title, description",
+    [
+        ("awesome new title", "awesome new content"),
+        ("favorite pizza", "i love pepperoni"),
+        ("tallest skyscrapers", "wahoo"),
+    ],
+)
+def test_create_entity(authorized_client, test_user, title, description):
+    res = authorized_client.post(
+        "/entities/", json={"title": title, "description": description}
+    )
+
+    created_entity = schemas.Entity(**res.json())
+    assert res.status_code == 201
+    assert created_entity.title == title
+    assert created_entity.description == description
+    assert created_entity.owner_id == test_user["id"]
+
+
+def test_unauthorized_user_create_entity(client):
+    res = client.post(
+        "/entities/", json={"title": "arbitrary title", "description": "aasdfjasdf"}
+    )
+    assert res.status_code == 401
+
+
+def test_unauthorized_user_delete_entity(client, test_entities):
+    res = client.delete(f"/entities/{test_entities[0].id}")
+    assert res.status_code == 401
+
+
+def test_delete_entity_success(authorized_client, test_entities):
+    res = authorized_client.delete(f"/entities/{test_entities[0].id}")
+
+    assert res.status_code == 204
+
+
+def test_delete_entity_non_exist(authorized_client):
+    res = authorized_client.delete("/entities/8000000")
+
+    assert res.status_code == 404
+
+
+def test_delete_other_user_entity(authorized_client, test_entities):
+    res = authorized_client.delete(
+        f"/entities/{test_entities[3].id}")
+    assert res.status_code == 403
+
+
+def test_update_entity(authorized_client, test_entities):
+    data = {
+        "title": "updated title",
+        "description": "updatd content",
+        "id": test_entities[0].id,
+    }
+    res = authorized_client.put(f"/entities/{test_entities[0].id}", json=data)
+    updated_entity = schemas.Entity(**res.json())
+    assert res.status_code == 200
+    assert updated_entity.title == data["title"]
+    assert updated_entity.description == data["description"]
+
+
+def test_update_other_user_entity(authorized_client, test_user, test_user2, test_entities):
+    data = {
+        "title": "updated title",
+        "description": "updatd content",
+        "id": test_entities[3].id
+
+    }
+    res = authorized_client.put(f"/entities/{test_entities[3].id}", json=data)
+    assert res.status_code == 403
+
+
+def test_unauthorized_user_update_entity(client, test_user, test_entities):
+    res = client.put(f"/entities/{test_entities[0].id}")
+    assert res.status_code == 401
+
+
+def test_update_entity_non_exist(authorized_client, test_user, test_entities):
+    data = {
+        "title": "updated title",
+        "description": "updatd content",
+        "id": test_entities[2].id
+
+    }
+    res = authorized_client.put("/entities/8000000", json=data)
+
+    assert res.status_code == 404
